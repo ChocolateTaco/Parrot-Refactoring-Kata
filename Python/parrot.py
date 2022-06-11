df23791995cea0ad3c051bcb5eca9750c499afd0
Steven Tran <trans4@oregonstate.edu>
2022-06-11 09:51:19 -0700

    Updated with getters and added unit tests

--- Python/parrot.py
+++ Python/parrot.py
@@ -6,7 +6,6 @@ class ParrotType(Enum):  # If it is not available, just remove it.
     AFRICAN = 2
     NORWEGIAN_BLUE = 3
 
-
 class Parrot:
 
     def __init__(self, type_of_parrot, number_of_coconuts, voltage, nailed):
@@ -14,25 +13,39 @@ class Parrot:
         self._number_of_coconuts = number_of_coconuts
         self._voltage = voltage
         self._nailed = nailed
+        self._load_factor = 9.0
+        self._base_speed = 12.0
 
     def speed(self):
-        if self._type == ParrotType.EUROPEAN:
-            return self._base_speed()
-        if self._type == ParrotType.AFRICAN:
-            return max(0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
-        if self._type == ParrotType.NORWEGIAN_BLUE:
-            if self._nailed:
+        if self.get_type() == ParrotType.EUROPEAN:
+            return self.get_base_speed()
+        if self.get_type() == ParrotType.AFRICAN:
+            return max(0, self.get_base_speed() - self.get_load_factor() * self.get_number_of_coconuts())
+        if self.get_type() == ParrotType.NORWEGIAN_BLUE:
+            if self.get_nailed_status():
                 return 0
             else:
-                return self._compute_base_speed_for_voltage(self._voltage)
+                return self.compute_base_speed_for_voltage(self.get_voltage())
 
         raise ValueError("should be unreachable")
 
-    def _compute_base_speed_for_voltage(self, voltage):
-        return min([24.0, voltage * self._base_speed()])
+    def compute_base_speed_for_voltage(self, voltage):
+        return min([24.0, voltage * self.get_base_speed()])
 
-    def _load_factor(self):
-        return 9.0
+    def get_load_factor(self):
+        return self._load_factor
 
-    def _base_speed(self):
-        return 12.0
+    def get_base_speed(self):
+        return self._base_speed
+
+    def get_voltage(self):
+        return self._voltage
+
+    def get_number_of_coconuts(self):
+        return self._number_of_coconuts
+
+    def get_type(self):
+        return self._type
+
+    def get_nailed_status(self):
+        return self._nailed
